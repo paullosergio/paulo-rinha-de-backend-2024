1695adb209508f02304397724776366b9e87e2b2
paulo.sergio <paulo.sergio@enacom.com.br>
2024-03-05 00:14:38 -0300

    second commit

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import time
 from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
+from sqlalchemy.future import select as future_select  # Improved for async
 
 from . import schemas
 from .database import SessionLocal
@@ -26,50 +27,48 @@ async def post_transacao(
     transacao: schemas.TransactionBase,
     session: Session = Depends(get_session),
 ):
-    # Query the cliente object from the session
-    cliente = session.get(Cliente, cliente_id)
 
-    # Check if cliente exists
+    cliente = session.get(Cliente, cliente_id) 
     if not cliente:
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
 
+    # Process transaction
     if transacao.tipo == "d":
         if cliente.saldo - transacao.valor < -cliente.limite:
             raise HTTPException(status_code=422, detail="Saldo insuficiente")
         cliente.saldo -= transacao.valor
     else:
         cliente.saldo += transacao.valor
-    
-    session.add(Transacao(**transacao.model_dump(),cliente_id=cliente_id))
+
+    session.add(Transacao(**transacao.model_dump(), cliente_id=cliente_id))
     session.commit()
-    
-    return {
-        "limite": cliente.limite,
-        "saldo": cliente.saldo,
-    }
+
+    return cliente
 
 
 @app.get("/clientes/{id}/extrato")
 async def get_extrato(id: int, session: Session = Depends(get_session)):
-    cliente = session.get(Cliente, id)
+    """Optimized statement for fetching the last 10 transactions."""
 
+    cliente = session.get(Cliente, id)
     if not cliente:
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
 
-    transactions = session.execute(
-        select(Transacao)
-        .where(Transacao.cliente_id == id)
-        .order_by(Transacao.id.desc()).limit(10)
+    transactions_query = (
+        future_select(Transacao)
+        .filter(Transacao.cliente_id == id)
+        .order_by(Transacao.id.desc())
+        .limit(10)
     )
-
+    transactions = session.execute(transactions_query)
 
     return {
         "saldo": {
             "total": cliente.saldo,
             "data_extrato": time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime()),
-            "limite": cliente.limite
+            "limite": cliente.limite,
         },
         "ultimas_transacoes": [
             schemas.Transaction.model_validate(t) for t in transactions.scalars()
-        ]
+        ],
     }
