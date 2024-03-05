e6de153f09d22f32806500b9d29743b9bc9385a9
paulo.sergio <paulo.sergio@enacom.com.br>
2024-03-05 09:45:47 -0300

    delete comment

--- script.sql
+++ script.sql
@@ -24,38 +24,6 @@ VALUES
     (10000000, 0),
     (500000, 0);
 
--- CREATE OR REPLACE FUNCTION update_balance()
--- RETURNS TRIGGER AS $$
--- DECLARE
---     v_saldo INTEGER;
---     v_limite INTEGER;
--- BEGIN
---     SELECT saldo, limite INTO v_saldo, v_limite
---     FROM clients WHERE id = NEW.cliente_id
---     FOR UPDATE;
-
---     IF NEW.tipo = 'd' AND (v_saldo - NEW.valor) < -v_limite THEN
---         RAISE EXCEPTION USING
---             errcode='23000',
---             message='Limite insuficiente para realizar débito',
---             hint='Realize um crédito para liberar mais limite';
---     END IF;
-
---     IF NEW.tipo = 'd' THEN
---         UPDATE clients SET saldo = saldo - NEW.valor WHERE id = NEW.cliente_id;
---     ELSE
---         UPDATE clients SET saldo = saldo + NEW.valor WHERE id = NEW.cliente_id;
---     END IF;
-
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
-
--- CREATE TRIGGER update_balance_trigger
--- AFTER INSERT ON transactions
--- FOR EACH ROW
--- EXECUTE FUNCTION update_balance();
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
